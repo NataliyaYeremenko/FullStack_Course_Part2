ff94b67de48c8a0d9a0efd0808f68ef98b718b19
N_Yeremenko <yeremenkonataliia@gmail.com>
2023-04-10 20:30:29 +0300

    Initial commit (HW1)

---
+++ Yeremenko_HW1/Yeremenko_HW1/Car.cs
@@ -0,0 +1,15 @@
+namespace Yeremenko_HW1
+{
+    public class Car
+    {
+        public string? Model { get; set; }
+
+        public int YearOfIssue { get; set; }
+
+        public string? Transmission { get; set; }
+
+        public string? TypeOfFuel { get; set; }
+
+        public int Mileage { get; set; }
+    }
+}
