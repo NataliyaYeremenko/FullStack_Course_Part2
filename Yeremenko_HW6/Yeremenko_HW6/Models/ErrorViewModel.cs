f9f6f9ad9eafd2d45fd77e5c0ced7b1fc8bc60e0
N_Yeremenko <yeremenkonataliia@gmail.com>
2023-04-27 20:49:03 +0300

    Initial commit (HW6)

---
+++ Yeremenko_HW6/Yeremenko_HW6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Yeremenko_HW6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
